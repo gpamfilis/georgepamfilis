21ccaf3586a26c90ab63546bcb1147935a77ac74
gpamfilis <gpamfilis@gmail.com>
2018-08-25 15:24:05 -0400

    removed flask datatables

--- manage.py
+++ manage.py
@@ -4,14 +4,14 @@ from app.models import User
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'pythonanywheree')
+app = create_app(os.getenv('FLASK_CONFIG') or 'pythonanywhere')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=User)
+
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
